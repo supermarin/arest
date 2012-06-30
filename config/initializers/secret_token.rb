a6f026ea77079096b10f12844b5da6a7a97a4e6f
Marin Usalj <mneorr@gmail.com>
2012-06-30 17:52:43 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BWHttp::Application.config.secret_token = '2af38aada2fb6429f3fcf5eb9102848f107493f945ace76af7533eda5b825ab091b3b2895ac2fd45094432b432f73ea31502ae89b62a456975be19c1c979f234'
