d08efdfe7c2d38734bee92b5ba2736c730ac9d8c
Marin Usalj <mneorr@gmail.com>
2012-07-04 11:58:56 +0200

    converted to spaces

--- app/controllers/http_controller.rb
+++ app/controllers/http_controller.rb
@@ -1,45 +1,45 @@
 class HttpController < ApplicationController
 
-	def get
-		render_params
-	end
+  def get
+    render_params
+  end
 
-	def post
-		render_params
-	end
+  def post
+    render_params
+  end
 
-	def put
-		render_params
-	end
+  def put
+    render_params
+  end
 
-	def head
-		render_params
-	end
+  def head
+    render_params
+  end
 
-	def patch
-		render_params
-	end
+  def patch
+    render_params
+  end
 
-	def delete
-		render_params
-	end
+  def delete
+    render_params
+  end
 
-	private
+  private
 
-	def render_params
-		prepare_output
-		respond_to do |wants|
-			wants.json { render :json => @output }
-			wants.xml { render :xml => @output }
-		end
-	end
+  def render_params
+    prepare_output
+    respond_to do |wants|
+      wants.json { render :json => @output }
+      wants.xml { render :xml => @output }
+    end
+  end
 
-	def prepare_output
-		params.delete(:controller)
-		params.delete(:format)
-		@output = {}
-		@output[:method] = params.delete(:action).upcase
-		@output[:params] = params
-	end
+  def prepare_output
+    params.delete(:controller)
+    params.delete(:format)
+    @output = {}
+    @output[:method] = params.delete(:action).upcase
+    @output[:params] = params
+  end
 
 end
